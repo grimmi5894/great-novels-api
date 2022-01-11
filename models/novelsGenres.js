1fcc8699c7015ec9155533b8e411693ebeb5235e
Tricia Brooker <grimmi5894@gmail.com>
2022-01-11 18:03:51 -0500

    added novelsGenres.js with model for novelsGenres linking table to models folder, imported genres and novels models to reference. exported.

---
+++ models/novelsGenres.js
@@ -0,0 +1,11 @@
+const genres = require('./genres')
+const novels = require('./novels')
+
+const novelsGenres = (connection, Sequelize, genres, novels) => {
+  return connection.define('novelsGenres', {
+    genreId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: genres, key: 'id' } },
+    novelId: { type: Sequelize.INTEGER, primaryKey: true, reference: { model: novels, key: 'id' } }
+  })
+}
+
+module.exports = novelsGenres
