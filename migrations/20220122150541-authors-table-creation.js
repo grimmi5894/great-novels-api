c7b82ba950c993acba7e5e53e6bdb6be86eba764
Tricia Brooker <grimmi5894@gmail.com>
2022-01-22 10:09:45 -0500

    created migration for authors table setup.

---
+++ migrations/20220122150541-authors-table-creation.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('authors', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      nameFirst: { type: Sequelize.STRING },
+      nameLast: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') },
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW() ON UPDATE NOW()') },
+      deletedAt: { type: Sequelize.DATE }
+    })
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable('authors')
+  }
+}
