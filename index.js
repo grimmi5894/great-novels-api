02f0fab90f09abae49365ba066da17a2a902542e
Tricia Brooker <grimmi5894@gmail.com>
2022-01-22 09:19:42 -0500

    created getAllNovels and getNovelById functions and exported. created get routes to retrieve all novels and novel by id. tested both routes with success.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { getAllAuthors, getAuthorById } = require('./controllers/authors')
 const { getAllGenres, getGenreById } = require('./controllers/genres')
+const { getAllNovels, getNovelById } = require('./controllers/novels')
 
 const app = express()
 
@@ -9,6 +10,9 @@ app.get('/authors/:id', getAuthorById)
 
 app.get('/genres', getAllGenres)
 app.get('/genres/:id', getGenreById)
+
+app.get('/novels', getAllNovels)
+app.get('/novels/:id', getNovelById)
 
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
