3dd00497ed2b6e9f1cd596e6372cdd6a7f2bf4da
Tricia Brooker <grimmi5894@gmail.com>
2022-01-22 09:08:11 -0500

    created getAllAuthors and getAuthorById functions, exported. added get routes to retrieve all authors and to retrieve an author and novels by id to index.js. tested both routes with success.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const { getAllAuthors, getAuthorById } = require('./controllers/authors')
 
 const app = express()
+
+app.get('/authors', getAllAuthors)
+app.get('/authors/:id', getAuthorById)
 
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
