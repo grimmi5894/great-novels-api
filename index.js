f5a96cd2ce507dbc52ced84419e5ef9436d94d83
Tricia Brooker <grimmi5894@gmail.com>
2022-01-22 09:16:12 -0500

    created getAllGenres and getGenreById functions and exported. created get routes to retrieve all genres and genre by id. tested both routes with success.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const { getAllAuthors, getAuthorById } = require('./controllers/authors')
+const { getAllGenres, getGenreById } = require('./controllers/genres')
 
 const app = express()
 
 app.get('/authors', getAllAuthors)
 app.get('/authors/:id', getAuthorById)
+
+app.get('/genres', getAllGenres)
+app.get('/genres/:id', getGenreById)
 
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
